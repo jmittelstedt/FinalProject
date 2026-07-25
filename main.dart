61f94fae7c5da67991fddd854cc5bcd30937294f
jmittelstedt <154637524+jmittelstedt@users.noreply.github.com>
2026-07-25 15:54:20 -0400

    Create main.dart

---
+++ main.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+import 'screens/login_screen.dart';
+
+
+void main() {
+
+ runApp(
+   const MyApp()
+ );
+
+}
+
+
+class MyApp extends StatelessWidget {
+
+ const MyApp({super.key});
+
+
+ @override
+ Widget build(BuildContext context){
+
+ return MaterialApp(
+
+ title:"Final Project",
+
+ debugShowCheckedModeBanner:false,
+
+ home: LoginScreen(),
+
+ );
+
+ }
+
+}
